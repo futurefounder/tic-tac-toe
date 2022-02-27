336671288619ffde7701f3ebfc26a6ac6e6ff7e6
f21 <jsk@posteo.de>
2022-02-27 15:28:13 -0600

    added new codebase by DCode

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// (s) DCode - https://github.com/dcode-youtube/tic-tac-toe-vanilla-js/
+
+import Game from "./Game.js";
+import GameView from "./GameView.js";
+
+let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
+
+// define functions
+
+gameView.onTileClick = function (i) {
+    game.makeMove(i);
+    gameView.update(game);
+};
+
+gameView.onRestartClick = function() {
+    game = new Game ();
+    gameView.update(game);
+};
+
+gameView.update(game);
